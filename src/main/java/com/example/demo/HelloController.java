af6b31218c6cf76c8c4d04bf93e5d4fbe09216e3
sky91win <sky91win@gmail.com>
2026-07-19 22:40:00 +0530

    final_new

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.*;
+@RestController
+public class HelloController{
+@GetMapping("/hello")
+public String hello(){ return "Hello from EKS!"; }
+}
